8003b36d9f54fab5b082d459cd2f662cf30ec75d
alexfridman <alexfridman@outlook.com>
2016-04-16 17:46:52 +0300

    Add get_by_id and is_resources_available methods

--- dgs/gsserver/db/gsresource.py
+++ dgs/gsserver/db/gsresource.py
@@ -7,3 +7,18 @@ class GSResource(me.Document):
     content = me.BinaryField()
     is_locked = me.BooleanField(default=False)
     is_deletion_requested = me.BooleanField()
+
+    @classmethod
+    def get_by_id(cls, resource_id):
+        try:
+            return GSResource.objects.get(resource_id=resource_id)
+        except me.DoesNotExist:
+            pass
+
+    @classmethod
+    def is_resources_available(cls, resource_ids):
+        for resource_id in resource_ids:
+            resource = cls.get_by_id(resource_id)
+            if not resource or resource.is_deletion_requested:
+                return False
+        return True
