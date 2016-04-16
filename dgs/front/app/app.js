d19f785618dfff7d761fe798554a0879b3cbcfa4
alexfridman <alexfridman@outlook.com>
2016-04-16 16:57:48 +0300

    Some front changes

--- dgs/front/app/app.js
+++ dgs/front/app/app.js
@@ -7,7 +7,7 @@ taskApp.controller("taskController", function ($scope, $http, $interval, $log) {
     };
 
     $scope.options = {
-        status: [
+        state: [
             'All',
             'Idle',
             'Running',
@@ -20,33 +20,33 @@ taskApp.controller("taskController", function ($scope, $http, $interval, $log) {
             'Date'
         ]
     };
-    ;
+
 
     $scope.defaultParams = {
-        status: 'All',
+        state: 'All',
         sort: 'Date',
         q: undefined,
         offset: 0,
         count: 5
     };
-    ;
+
 
     $scope.formParams = angular.copy($scope.defaultParams);
-    ;
+
     $scope.params = angular.copy($scope.defaultParams);
-    ;
+
 
     $scope.search = function () {
         $scope.params = angular.copy($scope.formParams);
-        ;
+
         $scope.update()
     };
-    ;
+
 
     $scope.reset = function () {
         $scope.formParams = angular.copy($scope.defaultParams)
     };
-    ;
+
 
     $scope.update = function () {
         $http({
@@ -59,11 +59,11 @@ taskApp.controller("taskController", function ($scope, $http, $interval, $log) {
             $log.warn('error loading data')
         });
     };
-    ;
+
 
     $scope.update();
-    ;
-    $interval($scope.update, 5000)
+
+    $interval($scope.update, 10000)
 
 
 });
@@ -77,7 +77,7 @@ taskApp.controller("createFormController", function ($scope, $uibModal, $log) {
 
         var modalInstance = $uibModal.open({
             animation: $scope.animationsEnabled,
-            templateUrl: 'partical/add-form.html',
+            templateUrl: 'myModalContent.html',
             controller: 'modalController',
             size: size,
             resolve: {
@@ -101,26 +101,46 @@ taskApp.controller("createFormController", function ($scope, $uibModal, $log) {
 });
 
 
-taskApp.controller('modalController', function ($scope, $uibModalInstance) {
+taskApp.controller('modalController', function ($scope, $uibModalInstance, $http, $log) {
 
-    /*$scope.ok = function () {
-     $uibModalInstance.close($scope.selected.item);
-     };*/
+    $scope.formParams = {
+        title: 'Custom task',
+        file: undefined
+    };
 
     $scope.cancel = function () {
         $uibModalInstance.dismiss('cancel');
     };
+
+    $scope.onFileSelect = function (files) {
+        $scope.formParams.file = files[0];
+    }
+
+    $scope.doAddTask = function () {
+        $http({
+            method: 'POST',
+            url: 'http://localhost:5000/add',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            transformRequest: function (data) {
+                var formData = new FormData();
+                formData.append("title", angular.toJson($scope.formParams.title));
+                formData.append("file", $scope.formParams.file);
+                return formData.toJSON();
+            }
+        }).then(function successCallback(response) {
+            alert("success!");
+        }, function errorCallback(response) {
+            alert("failed!");
+        });
+    };
+
+    $scope.addTask = function () {
+        $log.info('Sending data...');
+        $scope.doAddTask();
+
+
+        $uibModalInstance.dismiss('ok');
+    }
 });
-
-
-// Declare app level module which depends on views, and components
-/*angular.module('myApp', [
- 'ngRoute',
- 'myApp.view1',
- 'myApp.view2',
- 'myApp.version'
- ]).
- config(['$routeProvider', function($routeProvider) {
- $routeProvider.otherwise({redirectTo: '/view1'});
- }]);
- */
