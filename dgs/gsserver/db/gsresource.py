0bd1fce12571bb71cd78a9507753f7802a99b3a3
alexfridman <alexfridman@outlook.com>
2016-04-16 17:08:27 +0300

    Add GSResource

---
+++ dgs/gsserver/db/gsresource.py
@@ -0,0 +1,8 @@
+import mongoengine as me
+
+
+class GSResource(me.Document):
+    resource_id = me.StringField(primary_key=True)
+    name = me.StringField()
+    content = me.BinaryField()
+    is_locked = me.BooleanField(default=False)
