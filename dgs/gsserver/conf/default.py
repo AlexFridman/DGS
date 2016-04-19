88a1d46ceb588e9c353f931e2e6aff819c073b92
alexfridman <alexfridman@outlook.com>
2016-04-19 12:59:03 +0300

    Update config file

--- dgs/gsserver/conf/default.py
+++ dgs/gsserver/conf/default.py
@@ -1,20 +1,26 @@
 from dgs.gsserver.conf.conf import Celery, Mongo, Master, GSServerConf
 
 mongo_conf = {
-    'db': 'test',
-    'hosts': 'localhost:27017',
-    'username': 'alfrid_db',
-    'password': '1234'
+    'db': 'dgs',
+    'hosts': (),
+    'username': '',
+    'password': '',
+    'rs': 'dgs_rs'
 }
 
 master_conf = {
     'host': '::',
     'port': 5000
-
 }
+
+
+class TaskControllerConfig:
+    tick_interval = 1
+    wait_task_add_event = False
+
 
 mongo = Mongo(**mongo_conf)
 master = Master(**master_conf)
 celery = Celery(mongo)
 
-conf = GSServerConf(mongo, celery, master)
+conf = GSServerConf(mongo, celery, master, TaskControllerConfig)
